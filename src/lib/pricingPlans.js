bde79114b42d9d58c1b9944345ef33e14442ad7d
csconstruction43-hue <csconstruction43@gmail.com>
2026-09-10 18:16:50 +0530

    Update pricingPlans.js

--- src/lib/pricingPlans.js
+++ src/lib/pricingPlans.js
@@ -39,7 +39,7 @@ export const PLANS = {
       'Analytics dashboard',
       'Cancel anytime',
     ],
-    link: 'https://www.paypal.com/ncp/payment/ELWXQ4SMCK83E',
+    link: 'https://www.paypal.com/ncp/payment/LHXQWMGSZUMCJ',
     buttonText: 'Try now',
   },
   monthly: {
